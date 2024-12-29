8d4a6d3e38e009f0c584515beee11df0e9d795b1
guowanghushifu <guowanghushifu@gmail.com>
2024-12-29 18:01:08 +0800

    Update tcp_bbrz.c

--- BBR/BBRx/tcp_bbrz.c
+++ BBR/BBRx/tcp_bbrz.c
@@ -157,7 +157,7 @@ static const int bbr_high_gain  = BBR_UNIT * 2885 / 1000 + 1;
  */
 static const int bbr_drain_gain = BBR_UNIT * 1200 / 2885;
 /* The gain for deriving steady-state cwnd tolerates delayed/stretched ACKs: */
-static const int bbr_cwnd_gain  = BBR_UNIT * 3;
+static const int bbr_cwnd_gain  = BBR_UNIT * 2;
 /* The pacing_gain values for the PROBE_BW gain cycle, to discover/share bw: */
 static const int bbr_pacing_gain[] = {
     BBR_UNIT * 6 / 4,    /* probe for more available bw */
