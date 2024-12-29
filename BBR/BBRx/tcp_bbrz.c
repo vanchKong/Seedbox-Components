60c86ab17e4b0d79f10e94d63f0c338051fa71a5
guowanghushifu <guowanghushifu@gmail.com>
2024-12-29 19:42:13 +0800

    Update tcp_bbrz.c

--- BBR/BBRx/tcp_bbrz.c
+++ BBR/BBRx/tcp_bbrz.c
@@ -162,8 +162,8 @@ static const int bbr_cwnd_gain  = BBR_UNIT * 2;
 static const int bbr_pacing_gain[] = {
     BBR_UNIT * 6 / 4,    /* probe for more available bw */
     BBR_UNIT * 3 / 4,    /* drain queue and/or yield bw to other flows */
-    BBR_UNIT * 9 / 8, BBR_UNIT * 9 / 8, BBR_UNIT * 9 / 8,    /* cruise at 1.0*bw to utilize pipe, */
-    BBR_UNIT * 9 / 8, BBR_UNIT * 9 / 8, BBR_UNIT * 9 / 8    /* without creating excess queue... */
+    BBR_UNIT * 8 / 7, BBR_UNIT * 8 / 7, BBR_UNIT * 8 / 7,    /* cruise at 1.0*bw to utilize pipe, */
+    BBR_UNIT * 7 / 6, BBR_UNIT * 7 / 6, BBR_UNIT * 7 / 6    /* without creating excess queue... */
 };
 /* Randomize the starting gain cycling phase over N phases: */
 static const u32 bbr_cycle_rand = 7;
